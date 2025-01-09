7145ef3fa1a4c96dac6a165c841d7c3bb52be1fb
Yajat Jaiswal <yajat@ony1loan.com>
2025-01-09 13:31:11 -0800

    update Base url

--- src/baseURL.jsx
+++ src/baseURL.jsx
@@ -1,3 +1,4 @@
-export const BASE_URL = 
-// `https://api.qualoan.com`
-`http://localhost:8081`
+export const BASE_URL =
+  // `https://qualoan-staging.onrender.com`
+  // `https://api.qualoan.com`
+  `http://localhost:8081`;
