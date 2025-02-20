b59af74a14ad6a372d296b637f12b78f646ec2b5
Abhay <abhay@only1loan.com>
2025-02-20 09:38:29 +0530

    website live

--- src/baseURL.jsx
+++ src/baseURL.jsx
@@ -1,5 +1,5 @@
 export const BASE_URL =
   // `https://qualoan-staging.onrender.com/api/user`
-  // `https://api.qualoan.com/api/user`
-  `https://preprod-crm.api.qualoan.com/api/user`;
+  `https://api.qualoan.com/api/user`
+  // `https://preprod-crm.api.qualoan.com/api/user`;
 // `http://localhost:8080/api/user`;
