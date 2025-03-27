9bab0abfbd12361965fec92053f765bb72682ff6
krishnaonly1loan <krishna@only1loan.com>
2025-03-27 11:25:21 +0530

    add marketing schemas

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,22 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
-// import App from "./App.jsx";
-// import "./index.css";
-
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <Router>
-//       {" "}
-//       {/* Wrap your App in Router */}
-//       <App />
-//     </Router>
-//   </StrictMode>
-// );
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { LocalizationProvider } from "@mui/x-date-pickers"; // Import LocalizationProvider
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"; // Import the date adapter
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { HelmetProvider } from "react-helmet-async"; // Import HelmetProvider
 import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Router>
-        {/* Wrap your App in Router */}
-        <App />
-      </Router>
-    </LocalizationProvider>
+    <HelmetProvider>
+      {" "}
+      {/* Ensure HelmetProvider is added */}
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Router>
+          <App />
+        </Router>
+      </LocalizationProvider>
+    </HelmetProvider>
   </StrictMode>
 );
